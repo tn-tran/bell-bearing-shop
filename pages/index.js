4ac75bf888f34baebde9396c2cc097b759c2848d
Tien Tran <tientran2290@gmail.com>
2022-05-17 14:40:28 -0700

    Add UI for products & prices

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import { getProductsInCollection } from '../lib/shopify'
+import ProductList from '../components/ProductList'
+export default function Home({ products }) {
+  console.log(products)
   return (
     <div className='text-3xl'>
-      Hello from shopify next.js
+      <ProductList products={products}></ProductList>
     </div>
   )
+}
+export async function getStaticProps() {
+  const products = await getProductsInCollection()
+  return {
+    props: { products }, // will be passed to the page component as props
+  }
 }
