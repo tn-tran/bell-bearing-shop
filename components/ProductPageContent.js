3ea59c1ca98fa025f0ad0d094e97601128675f4a
Tien Tran <tientran2290@gmail.com>
2022-05-17 16:37:32 -0700

    Add Product page component

---
+++ components/ProductPageContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProductPageContent({ product }) {
+  return (
+    <div>{product.title}</div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { getProductsInCollection } from '../lib/shopify'
-import ProductList from '../components/ProductList'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { getProductsInCollection } from "../lib/shopify";
+import ProductList from "../components/ProductList";
 export default function Home({ products }) {
-  console.log(products)
+
   return (
     <div className='text-3xl'>
       <ProductList products={products}></ProductList>
     </div>
-  )
+  );
 }
 export async function getStaticProps() {
-  const products = await getProductsInCollection()
+  const products = await getProductsInCollection();
   return {
     props: { products }, // will be passed to the page component as props
-  }
+  };
 }
