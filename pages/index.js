d0d6fb50f23cc6eb5b33d242eee8d76698daed0a
Tien Tran <tientran2290@gmail.com>
2022-05-26 11:04:48 -0700

    Add Hero section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { getProductsInCollection } from "../lib/shopify";
-import ProductList from "../components/ProductList";
+import { getProductsInCollection } from "../lib/shopify"
+import ProductList from "../components/ProductList"
+import Hero from '../components/Hero'
+
 export default function Home({ products }) {
 
   return (
-    <div className='text-3xl'>
+    <div className=''>
+      <Hero />
       <ProductList products={products}></ProductList>
     </div>
   );
