313887ae239675f0d6fdb40f0619c3ebd6e40ebe
Tien Tran <tientran2290@gmail.com>
2022-05-26 08:43:52 -0700

    Add Swiper.js and sass

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import ShopProvider from "../context/shopContext";
 import { useRouter } from 'next/router'
+import 'swiper/scss'
+import 'swiper/scss/navigation'
+import 'swiper/scss/pagination'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
